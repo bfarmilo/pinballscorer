df922f62cfd8bda59154439cd1abc601e407537a
Bill Farmilo <bill@farmilostrategy.com>
2018-11-08 10:29:26 -0500

    changing config to serve from proper subdomain

--- preact.config.js
+++ preact.config.js
@@ -4,7 +4,7 @@
 export default function (config, env, helpers) {
   config.module.loaders.push({test:/\.mp3|m4a$/, loader: env.production ? 'file-loader' : 'url-loader'})
   if (env.production) {
-    config.output.publicPath = '/pinballscorer/'
+    config.output.publicPath = '/'
   } 
   //config.plugins.push(defineConstants({ PUBLIC_PATH }))
 }
